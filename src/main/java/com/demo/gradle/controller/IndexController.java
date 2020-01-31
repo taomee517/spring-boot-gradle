3fb396ccf3f906ad8bd6763e72c413f0e0fee76b
Administrator <taomee517@gmail.com>
2020-01-31 16:46:48 +0800

    测试浏览器json view插件

--- src/main/java/com/demo/gradle/controller/IndexController.java
+++ src/main/java/com/demo/gradle/controller/IndexController.java
@@ -1,15 +1,21 @@
 package com.demo.gradle.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/index")
 public class IndexController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String sayHello(){
-        return "Hello,Gradle!";
+    public ResponseEntity<Map<String,String>> sayHello(){
+        Map map = new HashMap();
+        map.put("index","Hello,Gradle!");
+        return ResponseEntity.ok(map);
     }
 }
