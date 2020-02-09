d922e1e8504dbfe3b59b1a57965f38d80abea10c
Administrator <taomee517@gmail.com>
2020-02-09 15:39:05 +0800

    加入logback日志配置

--- src/main/java/com/demo/gradle/controller/IndexController.java
+++ src/main/java/com/demo/gradle/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.demo.gradle.controller;
 
 import com.demo.gradle.entity.Info;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/virus")
 public class IndexController {
@@ -19,6 +21,7 @@ public class IndexController {
 
     @RequestMapping(value = "/info", method = RequestMethod.GET)
     public ResponseEntity<Map<String,Info>> getInfo(){
+        log.info("进入InfoController-info方法");
         Map infoMap = new HashMap();
         String province = info.getProvince();
         infoMap.put(province,info);
